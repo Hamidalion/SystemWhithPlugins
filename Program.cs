649396d67445c209cbb6459a0cded5bc9b7c069a
Illia Kozlov <fearofwar@mail.ru>
2021-03-09 19:25:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using PluginIface;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace RSDN_ORG
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles("plugins", "*.dll");
+
+            foreach (string item in files)
+            {
+                Assembly asm = Assembly.LoadFrom(item);
+
+                foreach (Type t in asm.GetExportedTypes())
+                {
+                    if (typeof(IPlugin).IsAssignableFrom(t))
+                    {
+                        IPlugin pi = (IPlugin)asm.CreateInstance(t.FullName);
+                        Console.WriteLine(pi.GetName());
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
